49b24ffbeb3a879c6dd71dee3f8f04b9e8a73f0a
damola ifatifo <ifatifod4@gmail.com>
2024-01-20 14:29:35 +0100

    my commit

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
 function sniff() {
-
   let all = nam.value;
-  let maily = pro.value
-  let call = phone.value
-  
-  if (all=="" && maily==""&&call=="") {
+  let maily = pro.value;
+  let call = phone.value;
+
+  if (all == "" && maily == "" && call == "") {
     error.innerHTML = "This field is required";
     err.innerHTML = "This field is required";
     errs.innerHTML = "This field is required";
     error.style.color = "#c3081b";
     err.style.color = "#c3081b";
     errs.style.color = "#c3081b";
-
-  }else if(maily==""){
-    err.innerHTML = "Email is Required";
-    err.style.color = "#c3081b";
-  }else if(call=""){
-    errs.innerHTML = "Phone number is Required";
-    errs.style.color = "#c3081b";
   }
-   else {
-    err.innerHTML = ""
-    errs.innerHTML = ""
-    error.innerHTML=""
+  else if (all == "" && maily !== "" && call !== ""){
+    error.innerHTML = "Name is Required";
+    error.style.color = "#c3081b";
+    errs.innerHTML = "";
+    err.innerHTML = "";
+
+  } 
+  else if (all !== "" && maily == "" && call == ""){
+    err.innerHTML = "This field is required";
+    errs.innerHTML = "This field is required";
+    errs.style.color = "#c3081b";
+    err.style.color = "#c3081b";
+    error.innerHTML = "";
+
+  } 
+  else if ( maily !== ""&& all == "" && call == ""){
+    error.innerHTML = "This field is required";
+    errs.innerHTML = "This field is required";
+    errs.style.color = "#c3081b";
+    error.style.color = "#c3081b";
+    err.innerHTML = "";
+
+  } 
+  else if ( call !== ""&& maily == ""&& all == "" ){
+    error.innerHTML = "This field is required";
+    err.innerHTML = "This field is required";
+    err.style.color = "#c3081b";
+    error.style.color = "#c3081b";
+    errs.innerHTML = "";
+
+  } 
+  else if (all !== "" && maily == "" && call !== ""){
+    err.innerHTML = " Email is Required";
+    err.style.color = "#c3081b";
+    error.innerHTML = "";
+    errs.innerHTML = "";
+
+  } 
+  else if (all !== "" && maily !== "" && call == ""){
+    errs.innerHTML = "Number is Required";
+    errs.style.color = "#c3081b";
+    error.innerHTML = "";
+    err.innerHTML = "";
+
+  } else {
+    err.innerHTML = "";
+    error.innerHTML = "";
+    errs.innerHTML = "";
     window.location.href = "select.html";
   }
 }
-function pres(){
+function pres() {
   window.location.href = "indexa.html";
 }
-function step(){
+function step() {
   window.location.href = "pick.html";
 }
-function next(){
+function next() {
   window.location.href = "finish.html";
 }
-function move(){
+function move() {
   window.location.href = "select.html";
 }
- setTimeout(() => {
-   card.style.display="none"
- }, 4000);
-
-
+setTimeout(() => {
+  card.style.display = "none";
+}, 4000);
